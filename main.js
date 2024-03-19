43ca76c99e63043fb3a6a5243174ada784811158
Diana Papp <pappdiana148@gmail.com>
2024-03-19 15:43:17 +0200

    added a new js class file

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log('race game');
+
+const newPlayer = new Player('Petrucci', 'img/piggy.png');
+const newPlayer2 = new Player('Boberinho', 'img/beaver.png');
+
+console.log(newPlayer,newPlayer2);
---
+++ player.js
@@ -0,0 +1,10 @@
+class Player{
+   constructor(name,img){
+     this.name = name;
+     this.img = img;
+     this.score = 0;
+   }
+}
+
+
+
